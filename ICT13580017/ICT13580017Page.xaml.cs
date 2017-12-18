1cef4455cfe62d817f578a79d74af42c1a2ad046
CodeBangkok <surasuk612@gmail.com>
2017-12-18 16:58:24 +0700

    13580017A
    Project1

---
+++ ICT13580017/ICT13580017Page.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13580017
+{
+    public partial class ICT13580017Page : ContentPage
+    {
+        public ICT13580017Page()
+        {
+            InitializeComponent();
+
+            okButton.Clicked +=OkButton_Clicked;
+            genderPicker.Items.Add("ชาย");
+            genderPicker.Items.Add("หญิง");
+
+            mySlider.ValueChanged += MySlider_ValueChanged;
+            myStepper.ValueChanged += MyStepper_ValueChanged;
+        }
+
+        void OkButton_Clicked(object sender, System.EventArgs e)
+        {
+            firstNameLabel.Text = firstNameEntry.Text;
+
+        }
+
+        void MySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+            valueLabel.Text = value.ToString();
+        }
+
+        void MyStepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+            valueLabel.Text = value.ToString();
+        }
+    }
+}
